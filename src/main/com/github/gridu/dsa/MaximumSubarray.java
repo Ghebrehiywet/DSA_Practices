06427f9a8023e329146aa9373ad1a7bfd0455913
Ghebrehiywet <g.hiwetmit@gmail.com>
2023-05-04 09:39:59 -0400

    Maximum subarray solution is updated!

--- src/main/com/github/gridu/dsa/MaximumSubarray.java
+++ src/main/com/github/gridu/dsa/MaximumSubarray.java
@@ -2,12 +2,16 @@ package com.github.gridu.dsa;
 
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        int maxSoFar = nums[0];
-        int maxEndingHere = nums[0];
+        int tempBest = nums[0];
+        int best = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
-            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+            if (tempBest + nums[i] > nums[i])
+                tempBest += nums[i];
+            else
+                tempBest = nums[i];
+            if (best < tempBest)
+                best = tempBest;
         }
-        return maxSoFar;
+        return best;
     }
 }
